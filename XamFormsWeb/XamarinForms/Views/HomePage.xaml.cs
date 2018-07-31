e207235d2d05c4048c5c270014414b7a205ae8fc
Ryan Harding <ryanharding@outlook.com>
2018-07-31 16:39:25 +0200

    Ooui aspnet core test

---
+++ XamFormsWeb/XamarinForms/Views/HomePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamFormsWeb.XamarinForms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+
+            load();
+        }
+
+        private async Task load()
+        {
+            buttonX.Text = "hi";
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            buttonX.Text = "yay";
+        }
+
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var button = sender as Button;
+
+            if (button == null)
+                return;
+
+            count++;
+            button.Text = $"You clicked {count} times!";
+        }
+    }
+}
